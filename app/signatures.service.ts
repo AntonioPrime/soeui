ac73f631319fa9ad8b8d2582458ba7f70c1d0249
AntonioPrime <AntonioPrime@ya.ru>
2016-12-20 19:56:52 +0300

    fix signatures remove mock token

--- app/signatures.service.ts
+++ app/signatures.service.ts
@@ -1,24 +1,26 @@
-import {Injectable} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class SignaturesService {
   private url: string = 'http://' + window.location.hostname + ':8080' + '/ajax/signatures';
-  private headers = new Headers({
-    'Content-Type' : 'application/json',
-    'token' : 'YHYbqjFxevvL9LiHTEnP5GKB_5RcE1s95NMvTduvJLMN65ROjMF60YT6cZN-qqISPkz2vBNOUgcsMyrQM9hMUg2'});
-  private options = new RequestOptions({ headers: this.headers });
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   replace(fresh: string, locationId: number): Promise<any> {
+    var token = localStorage.getItem('token');
+    var headers = new Headers({
+      'Content-Type': 'application/json',
+      'token': token
+    });
+    var options = new RequestOptions({ headers: headers });
     return this.http.post(this.url, JSON.stringify(
       {
         fresh: fresh,
         locationId: locationId
-      }), this.options)
+      }), options)
       .toPromise();
   }
 }
